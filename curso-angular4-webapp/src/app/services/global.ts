a6b179d78f86ceba3de598b662124f612018d78b
david.garcia.dev <david.garcia.dev@gmail.com>
2017-07-31 03:18:01 +0200

    global.ts

--- curso-angular4-webapp/src/app/services/global.ts
+++ curso-angular4-webapp/src/app/services/global.ts
@@ -1,4 +1,5 @@
 export var GLOBAL = {
-	url: 'http://localhost/curso-angular4-backend/index.php/',
-	header_color: 'blue'
+	url: 'http://localhost/curso-angular4-backend/index.php/'
+	//,
+	//header_color: 'blue'
 }
