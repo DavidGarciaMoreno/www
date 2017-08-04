7f3532f7744e86b4c289a2e2481b0a0788cf1c7c
David García Moreno <david.garcia.dev@gmail.com>
2017-08-04 21:35:15 +0200

    Clase 7

---
+++ simpleCRUD/app/products/product.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { Product } from './product';
+import { ProductService } from './product.service';
+
+@Component({
+    moduleId: module.id,
+    templateUrl: 'product.template.html'
+})
+export class ProductComponent implements OnInit {
+	products: Product[];
+
+	constructor(private _productService: ProductService) {
+
+	}
+
+	ngOnInit() {
+		this.getProducts();
+	}
+
+	getProducts() {
+		this.products = this._productService.getProductsFromData();
+	}
+}
