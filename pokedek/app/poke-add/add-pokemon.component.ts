6b876b7d7ed43a2ca682568feef2ba3d85da7970
david.garcia.dev <david.garcia.dev@gmail.com>
2017-08-07 16:29:14 +0200

    Clase 22

--- pokedek/app/poke-add/add-pokemon.component.ts
+++ pokedek/app/poke-add/add-pokemon.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
 import { Pokemon } from '../shared/pokemon';
 import { PokemonService } from '../shared/pokemon.service';
-
 
 @Component({
 	moduleId: module.id,
 	templateUrl: 'add-pokemon.template.html'
 })
 export class AddPokemonComponent {
+  formPokemon: any = {};
+  cardTitle: string = 'Add Pokemon';
+  errorMessage: string;
+
+  constructor(private _pokemonService: PokemonService,
+              private _router: Router) { }
 
 }
