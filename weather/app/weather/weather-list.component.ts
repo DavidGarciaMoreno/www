ffcbeab8c1e8ff2dcec498c3400a6a413bd1fc2d
david.garcia.dev <david.garcia.dev@gmail.com>
2017-08-12 13:35:24 +0200

    Temp

--- weather/app/weather/weather-list.component.ts
+++ weather/app/weather/weather-list.component.ts
@@ -1,14 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { IWeather } from './weather';
+import { WeatherService } from './weather.service';
 
 @Component({
   moduleId: module.id,
   selector: 'we-list',
-  templateUrl: 'weather-item.template.html'
+  templateUrl: 'weather-list.template.html'
 })
 export class WeatherListComponent implements OnInit {
-  constructor() {}
+  weatherItems: IWeather[];
 
-  ngOnInit() {
-    
+  constructor(private _weatherService: WeatherService) {}
+
+  ngOnInit(): void {
+    this.getWeatherItems();    
+  }
+
+  getWeatherItems(): void {
+    this.weatherItems = this._weatherService.getWeatherItems();
   }
 }
