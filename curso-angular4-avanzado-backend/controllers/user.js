dacf7b5b8643b6608b73b1231cc6fbbc8307f3c7
David García Moreno <david.garcia.dev@gmail.com>
2017-09-04 22:21:55 +0200

    Clase 40

--- curso-angular4-avanzado-backend/controllers/user.js
+++ curso-angular4-avanzado-backend/controllers/user.js
@@ -28,21 +28,32 @@ function saveUser(req, res) {
 		user.role = 'ROLE_USER';
 		user.image = null;
 
-		
-		bcrypt.hash(params.password, null, null, function(err, hash) {
-			user.password = hash;
-			// guardar user en bd
-			user.save((err, userStored) => { 
-				if(err) {
-					res.status(500).send({ message: 'Error al guardar el usuario' });
+		User.findOne({ email: user.email.toLowerCase() }, (err, issetuser) =>{
+			if(err) {
+				res.status(500).send({ message: 'Error al comprobar el usuario' });				
+			} else {
+				if(!issetuser) {
+					bcrypt.hash(params.password, null, null, function(err, hash) {
+						user.password = hash;
+						// guardar user en bd
+						user.save((err, userStored) => { 
+							if(err) {
+								res.status(500).send({ message: 'Error al guardar el usuario' });
+							} else {
+								if(!userStored) {
+									res.status(404).send({ message: 'No se ha registrado el usuario' });
+								} else {
+									res.status(200).send({ user: userStored });
+								}
+							}
+						});
+					});						
 				} else {
-					if(!userStored) {
-						res.status(404).send({ message: 'No se ha registrado el usuario' });
-					} else {
-						res.status(200).send({ user: userStored });
-					}
+					res.status(200).send({
+						message: 'El usuario no puede registrarse'
+					});
 				}
-			});
+			}
 		});
 	} else {
 		res.status(200).send({
@@ -51,8 +62,37 @@ function saveUser(req, res) {
 	}
 }
 
+function login(req, res) {
+	var params = req.body;
+	var email = params.email;
+	var password = params.password;
+
+	User.findOne({ email: email.toLowerCase() }, (err, user) => {
+		if(err) {
+			res.status(500).send({ message: 'Error al comprobar el usuario' });				
+		} else {
+			if(user) {
+				bcrypt.compare(password, user.password, (err, check) => {
+					if(check) {
+						res.status(200).send({ user });		
+					} else {
+						res.status(404).send({
+							message: 'La contraseña no es válida'
+						});
+					}
+				});
+			} else {
+				res.status(404).send({
+					message: 'El usuario no ha podido hacer login'
+				});
+			}
+		}
+	});
+}
+
 module.exports = {
 	// exportamos los metodos
 	pruebas,
-	saveUser
+	saveUser,
+	login
 };
